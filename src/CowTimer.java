0e00c518c063f272eeaf38d38465eaf95dfdd4e5
audreychan <league@wts14-4.attlocal.net>
2019-02-23 11:27:19 -0800

    Worked on MagicBox

--- src/CowTimer.java
+++ src/CowTimer.java
@@ -1,10 +1,11 @@
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2019
  *    Level 1
  */
 
 import java.applet.AudioClip;
-import java.io.IOException;
+
 import javax.swing.JApplet;
 
 public class CowTimer {
@@ -18,34 +19,33 @@ public class CowTimer {
 		CowTimer timer = new CowTimer();
 		timer.setTime(3);
 		timer.start();
-			}
+	}
 
 	private int minutes;
+
 	public void setTime(int minutes) {
 		this.minutes = minutes;
 		System.out.println("Cow set to " + minutes + " minutes.");
 	}
 
 	public void start() throws InterruptedException {
-		
-			
 
 		/*
 		 * 2. Count down the minutes, print the current minute then sleep for 60 seconds
 		 * using Thread.sleep(int milliseconds).
 		 */
-		System.out.println(minutes*60);
-	for (int i = minutes*60; i >=1; i--) {
-		
-		Thread.sleep(10);
-			System.out.println(i%60);
-		if(i%60==0) {
-		System.out.println("minute");
+		System.out.println(minutes * 60);
+		for (int i = minutes * 60; i >= 1; i--) {
+
+			Thread.sleep(1000);
+			System.out.println(i % 60);
+			if (i % 60 == 0) {
+				System.out.println("minute");
+			}
+			if (i == 1) {
+				playSound("moo.wav");
+			}
 		}
-		if(i==1) {
-			playSound("moo.wav");
-		}
-	}
 		/*
 		 * 3. When the timer is finished, use the playSound method to play a moo sound.
 		 * You can use the .wav file in the default package, or you can download one
--- src/MagicBox.java
+++ src/MagicBox.java
@@ -1,3 +1,4 @@
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2018
  *    Level 1
@@ -11,7 +12,6 @@ import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 import javax.imageio.ImageIO;
@@ -27,15 +27,14 @@ import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
 public class MagicBox extends JPanel implements Runnable, MouseListener {
-JLabel label= new JLabel();
+	JLabel label = new JLabel();
 	/*
 	 * We are going to hide secrets within the magic box. When the user clicks on a
 	 * secret place, stuff will happen.
 	 * 
-	 * 1. Make the frame respond to mouse clicks.
-	 * 
-	 * 2. When the mouse is clicked, use the Media Palace (read the code in the
-	 * default package) to play sounds, show images or speak.
+	 * 1. Make the frame respond to mouse clicks. 2. When the mouse is clicked, use
+	 * the Media Palace (read the code in the default package) to play sounds, show
+	 * images or speak.
 	 * 
 	 * 3. backgroundImage.getRGB(keyEvent.getX(), keyEvent.getY()) will give you the
 	 * color of the current pixel.
@@ -47,16 +46,19 @@ JLabel label= new JLabel();
 		SwingUtilities.invokeLater(new MagicBox());
 
 	}
-JFrame frameTwo= new JFrame();
+
+	JFrame frameTwo = new JFrame();
+	AudioClip clip;
 
 	@Override
 	public void run() {
 		try {
-			loadBackgroundImage();
+			loadBackgroundImage("src/magic-box.jpg");
 			createUI();
 		} catch (Exception w) {
 			System.err.println(w.getMessage());
 		}
+
 	}
 
 	private void createUI() {
@@ -67,24 +69,62 @@ JFrame frameTwo= new JFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 		frame.addMouseListener(this);
+		label = loadImageFromWithinProject("InsertRandomImage.png");
+		label.setVisible(false);
+		frame.add(label);
+		JFrame frame2 = new JFrame();
+		frame2.setVisible(false);
+		frame2.add(label);
+		clip = loadSound("Clock tick tock SOUND EFFECTS.wav");
+
 	}
 
 	public AudioClip loadSound(String fileName) {
 		return JApplet.newAudioClip(getClass().getResource(fileName));
 	}
-	private void loadBackgroundImage() throws Exception {
-		String imageFile = "src/magic-box.jpg";
+
+	private void loadBackgroundImage(String imageFile) throws Exception {
+
 		try {
 			backgroundImage = ImageIO.read(new File(imageFile));
 		} catch (IOException e) {
 			throw new Exception("Could not load image: " + imageFile);
 		}
 	}
+
+	public void playSoundFromInternet(String soundURL) {
+		try {
+			AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new URL(soundURL));
+			Clip clip = AudioSystem.getClip();
+			clip.open(audioInputStream);
+			clip.start();
+			Thread.sleep(500);
+		} catch (Exception ex) {
+			System.out.println("Problem playing sound: " + soundURL);
+			ex.printStackTrace();
+		}
+	}
+
 	public JLabel loadImageFromWithinProject(String fileName) {
 		URL imageURL = getClass().getResource(fileName);
 		Icon icon = new ImageIcon(imageURL);
 		return new JLabel(icon);
 	}
+
+	public void playMusicOnComputer(String fileName) {
+		File fileToPlay = new File(fileName);
+		try {
+			java.awt.Desktop.getDesktop().open(fileToPlay);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+	}
+
+	public JLabel loadImageFromHardDrive(String filePath) {
+		Icon icon = new ImageIcon(filePath);
+		return new JLabel(icon);
+	}
+
 	@Override
 	public void paintComponent(Graphics g) {
 		g.drawImage(backgroundImage, 0, 0, null);
@@ -93,9 +133,15 @@ JFrame frameTwo= new JFrame();
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		// TODO Auto-generated method stub
-	System.out.println("clicked");
-	label= loadImageFromWithinProject("ShaggyGod.jpeg");
-	
+		System.out.println("clicked");
+		clip.play();
+		try {
+			loadBackgroundImage("src/InsertRandomImage.png");
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		repaint();
 	}
 
 	@Override
